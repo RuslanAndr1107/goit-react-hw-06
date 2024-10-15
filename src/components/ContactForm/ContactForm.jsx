224a038258d143e57e00526fcd915e361a31c0f3
RuslanAndr1107 <androshulikruslan@gmail.com>
2024-10-15 11:40:08 +0300

    name fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { addContact, selectContacts } from '../../redux/contactSlice';
+import { addContact, selectContacts } from '../../redux/contactsSlice';
 
 const ContactsForm = () => {
   const dispatch = useDispatch();
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; 
-import contactReducer from './contactSlice';
+import contactReducer from './contactsSlice';
 import filterReducer from './filterSlice';
 import { combineReducers } from 'redux';
 import {
